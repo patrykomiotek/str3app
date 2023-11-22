841613646320744aa8edfbef12a9ac5b80ae7914
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-22 12:36:18 +0100

    feat: generator component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 // import { Text as MyText } from "./ui";
 import { Button, Text } from "./ui";
+import { Generator } from "./components/Generator";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Text>Today is Wednesday</Text>
       <Button label="Click me" />
       {/* <Button label="Click me" bgColor="midnight-blue" color="clouds" /> */}
+      <Generator />
     </>
   );
 }
---
+++ src/components/Generator/Generator.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Button } from "../../ui";
+
+export const Generator = () => {
+  const [id, setId] = useState(uuidv4()); // arg -> defaultValue
+  // state[0] - value
+  // state[1] - setter
+
+  // let id = uuidv4();
+  const handleClick = () => {
+    // id = uuidv4();
+    setId(uuidv4());
+  };
+
+  return (
+    <div>
+      <p>{id}</p>
+      <Button label="Refresh" onClick={handleClick} />
+    </div>
+  );
+};
