681eff0613e455cc05cfcbd561ec2db453852f47
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-23 12:29:50 +0100

    feat: styles for menu and footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div>
+    <div className="mt-8">
       <p>&copy; 2023</p>
     </div>
   );
--- src/components/Layout/Menu.tsx
+++ src/components/Layout/Menu.tsx
@@ -4,18 +4,18 @@ import { Route } from "../../routes";
 export const Menu = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
+      <nav className="mb-8">
+        <ul className="flex list-none">
+          <li className="mr-3">
             <Link to={Route.HOME.path}>Home</Link>
           </li>
-          <li>
+          <li className="mr-3">
             <Link to={Route.PRODUCT_LIST.path}>Products</Link>
           </li>
-          <li>
+          <li className="mr-3">
             <Link to={Route.ABOUT.path}>About</Link>
           </li>
-          <li>
+          <li className="mr-3">
             <Link to={Route.CONTACT.path}>Contact</Link>
           </li>
         </ul>
