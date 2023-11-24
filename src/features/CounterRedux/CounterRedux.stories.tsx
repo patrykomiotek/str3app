259ffcb67fd28dd96043ff516a129f7cae4ff5a2
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-24 10:36:46 +0100

    feat: Cstory for CounterRedux component

---
+++ src/features/CounterRedux/CounterRedux.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta } from "@storybook/react";
+
+import { CounterRedux } from "./CounterRedux";
+import { Provider } from "react-redux";
+import { store } from "../../store";
+
+const meta = {
+  title: "Components/CounterRedux",
+  component: CounterRedux,
+  // tags: ["autodocs"],
+} satisfies Meta<typeof CounterRedux>;
+
+export default meta;
+
+export const _CounterRedux = () => (
+  <Provider store={store}>
+    <CounterRedux />
+  </Provider>
+);
