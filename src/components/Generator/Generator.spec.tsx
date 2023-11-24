fa2f868b764a7a79b691283188a133c665967a75
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-24 12:46:39 +0100

    feat: generator test

--- src/components/Generator/Generator.spec.tsx
+++ src/components/Generator/Generator.spec.tsx
@@ -1,11 +1,20 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Generator } from ".";
 
 describe("Generator component", () => {
   // test('')
   it("should generate different values", () => {
+    // getByLabel
     const { debug } = render(<Generator />);
     debug();
+    const id = screen.getByText(/[a-z0-9-]{36}/i).textContent;
+
+    // console.log(id);
+    fireEvent.click(screen.getByRole("button"));
+
+    const newId = screen.getByText(/[a-z0-9-]{36}/i).textContent;
+
+    expect(newId).not.toBe(id);
   });
 
   it.todo("should generate different values");
